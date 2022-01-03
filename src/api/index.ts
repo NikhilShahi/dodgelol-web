6117c6feb3b000e91361838c57359db3f0c12da1
Nikhil Shahi <nshahi1998@gmail.com>
2022-01-02 22:54:44 -0600

    use env variable

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,14 @@
-import { DOMAIN_NA1, RIOT_API_KEY } from "constants/api";
+import { DOMAIN_NA1 } from "constants/api";
 
 /*export async function fetchSummonerData(
   summonerName?: string
 ): Promise<Summoner> {
   const response = await fetch(
-    `${DOMAIN_NA1}/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${RIOT_API_KEY}`
+    `${DOMAIN_NA1}/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.REACT_APP_RIOT_API_KEY}`
   );
   return await response.json();
 }*/
 
 export const summonerDataURL = (summonerName?: string): string => {
-  return `${DOMAIN_NA1}/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${RIOT_API_KEY}`;
+  return `${DOMAIN_NA1}/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.REACT_APP_RIOT_API_KEY}`;
 };
--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,2 @@
 export const DOMAIN_NA1: string = "https://na1.api.riotgames.com";
 export const DOMAIN_NA1_TAG_LINE: string = "NA1";
-export const RIOT_API_KEY: string =
-  "RGAPI-9a46ee01-0018-49c0-9326-071f3afd15ac";
